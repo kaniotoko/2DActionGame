81cb7da5e5e4df19a870946c0188604ac467f129
kaniashi <s1320041@u-aizu.ac.jp>
2026-03-10 16:30:12 +0900

    PlayerCtrlに地面でのみジャンプ可能という機能を追加する

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -1,23 +1,31 @@
 using UnityEngine;
-using UnityEngine.InputSystem; // 1. これを追加
+using UnityEngine.InputSystem;
 
 public class PlayerCrtl : MonoBehaviour
 {
     Rigidbody2D rb;
+    CircleCollider2D coll;
     public float jumpPower;
+    
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<CircleCollider2D>();
     }
 
     void Update()
     {
-        // 2. 新しいInput Systemの書き方に変更
-        if(Keyboard.current.spaceKey.wasPressedThisFrame) 
+        RaycastHit2D groundHit = Physics2D.Raycast(transform.position + (Vector3)coll.offset, Vector2.down, coll.radius + 0.1f, LayerMask.GetMask("Ground"));
+
+        if(groundHit)
         {
-            rb.linearVelocityY = 0;
-            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            if(Keyboard.current.spaceKey.wasPressedThisFrame) 
+            {
+                rb.linearVelocityY = 0;
+                rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            }
         }
+        
     }
 }
