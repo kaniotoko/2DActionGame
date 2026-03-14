4c7a708e04f34cf12a3f03ad735bd43496f6eadb
kaniashi <s1320041@u-aizu.ac.jp>
2026-03-14 15:34:52 +0900

    プレイヤーのx座標にMainCameraがついてくるようにするスクリプトCameraCtrlを作成した

---
+++ Assets/Scripts/CameraCtrl.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+    public Transform player; //Transform型関数では位置情報を扱う
+    Vector3 startPos; //最初の座標
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, startPos.y, startPos.z);
+    }
+}
