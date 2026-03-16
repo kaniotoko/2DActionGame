d2f3e36b5913e22ef07b4b9a5f205dc6d06f7955
kaniashi <s1320041@u-aizu.ac.jp>
2026-03-16 15:31:30 +0900

    スタートシーンからメインシーンにボタンで移り変わる機能を持つStartManagerを追加した

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
